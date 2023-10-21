205a582624f99c09ea9a34686a3fa4df39060660
RASMRAHUL <rahulkumarsingh110022@gmail.com>
2023-10-21 15:34:51 +0530

    create a middleware and funtion to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../modals/user.model.js';
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) =>{
+export const signup = async (req, res, next) =>{
     
     const {username, email, password} = req.body;
     const hashPassword =  bcrypt.hashSync(password, 10);
@@ -11,10 +12,7 @@ export const signup = async (req, res) =>{
         await newUser.save();
         res.status(201).json("User creater successfully.");
     }catch(err){
-        res.status(500).json({
-            message : "User not created.",
-            error : err.message,
-        })
+        next(error);
     }
 
 };
